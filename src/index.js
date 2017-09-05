68b9229cb77e72ee49c9e2a7d88ae65c3fa90818
Brandon Walden <brandonshanewalden@gmail.com>
2017-09-05 12:52:48 -0400

    created components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './styles/index.css';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
